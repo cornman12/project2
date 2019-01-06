a337dc569338c4f8314daadb96db2acc5aa1818f
gin <gin@anhmys-air.attlocal.net>
2019-01-06 07:51:25 -0500

    add files

--- src/main/app.go
+++ src/main/app.go
@@ -13,23 +13,100 @@ import (
 )
 
 func main() {
+	
 	chapterchan := make(chan Chapter)
-	fmt.Println("hello world")
-	go getRequest("http://www.nettruyen.com/truyen-tranh/anh-hung-ta-khong-lam-lau-roi", chapterchan)
+
+	manga := Manga{"anh-hung-ta-khong-lam-lau-roi","http://www.nettruyen.com/truyen-tranh/anh-hung-ta-khong-lam-lau-roi"}
+	
+	
+	go getChapterList(manga, chapterchan)
+
 
 	for chap := range chapterchan {
 
 		fmt.Println(chap)
+		//go getChapter(chap)
 	}
+	
+
+	fmt.Scanln()
+
+	//getChapter("http://www.nettruyen.com/truyen-tranh/anh-hung-ta-khong-lam-lau-roi/chap-1/409746")
+
+
 
 }
 
+
+type Manga struct{
+	name string
+	url string
+}
+
+
+
 type Chapter struct {
+	mangaName string
 	name string
 	url  string
 }
 
-func getRequest(url string, chapterchan chan Chapter) {
+
+
+func getChapter(mangaChapter Chapter ) {
+
+	mangaName := mangaChapter.mangaName
+	chapterName := mangaChapter.name
+	url := mangaChapter.url
+	pwd, _ := os.Getwd()
+
+	fmt.Println("Getting images from "+ url)
+	httpconfig := &http.Transport{
+		MaxIdleConns:       10,
+		IdleConnTimeout:    10 * time.Second,
+		DisableCompression: true,
+	}
+	httpclient := &http.Client{Transport: httpconfig}
+	resp, err := httpclient.Get(url)
+	defer resp.Body.Close()
+	if err !=nil{
+		return
+	}
+	htmltoken := html.NewTokenizer(resp.Body)
+	for{
+
+		tokenTag := htmltoken.Next()
+		if tokenTag != html.ErrorToken{
+			nextToken := htmltoken.Token()
+			if nextToken.Data == "img"{
+				
+				for _, att := range nextToken.Attr{
+					if att.Key =="data-original"{
+						fmt.Println(nextToken.Attr[1].Val)
+						fmt.Println(nextToken.Attr[2].Val)
+					}
+				}
+			}
+
+		}else{
+			return
+		}
+
+
+	}
+
+}
+
+
+
+
+
+
+
+
+func getChapterList(manga Manga, chapterchan chan Chapter) {
+	mangaName:= manga.name
+	url := manga.url
 	chapterURLChannel := make(chan string, 1)
 	httpconfig := &http.Transport{
 		MaxIdleConns:       10,
@@ -52,7 +129,7 @@ func getRequest(url string, chapterchan chan Chapter) {
 			if tt.String() == "Text" && chapterelement == 1 {
 				chapterelement = 0
 				chapterurl := <-chapterURLChannel
-				chapter := Chapter{t.Data, chapterurl}
+				chapter := Chapter{mangaName ,t.Data, chapterurl}
 				chapterchan <- chapter
 				//fmt.Println(t.Data)
 				//fmt.Println(chapterurl)
